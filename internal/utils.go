14f61e0c839589a3b2573c3d90eae13bb96e5836
Carlo Alberto Ferraris <cafxx@strayorange.com>
2019-04-18 05:58:22 +0900

    add Bw2bs

--- internal/utils.go
+++ internal/utils.go
@@ -38,3 +38,31 @@ func (d *doroboReader) Read(buf []byte) (int, error) {
 	d.b = buf
 	return len(buf), nil
 }
+
+// Bw2bs extracts the backing slice from a bufio.Writer
+// The bufio.Writer can not be used and must be discarded when this function returns.
+func Bw2bs(w *bufio.Writer) []byte {
+	d := doroboWriter{}
+	w.Reset(&d)
+	zeros := _zeros[:]
+	if w.Size() < len(zeros) {
+		zeros = zeros[:w.Size()]
+	}
+	for d.b == nil {
+		w.Write(zeros)
+	}
+	w.Reset(nil)
+	return d.b
+}
+
+type doroboWriter struct {
+	io.Writer
+	b []byte
+}
+
+func (d *doroboWriter) Write(buf []byte) (int, error) {
+	d.b = buf
+	return len(buf), nil
+}
+
+var _zeros = [256]byte{}
