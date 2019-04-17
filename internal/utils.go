54e98eab8f79a4c3dc293d50f6236ab6bcd59f44
Carlo Alberto Ferraris <cafxx@strayorange.com>
2019-04-17 21:50:42 +0900

    add Br2bs

--- internal/utils.go
+++ internal/utils.go
@@ -1,6 +1,10 @@
 package internal
 
-import "bytes"
+import (
+	"bufio"
+	"bytes"
+	"io"
+)
 
 // Bb2bs extracts the backing slice from a bytes.Buffer.
 // The bytes.Buffer can not be used and must be discarded when this function returns.
@@ -13,4 +17,24 @@ func Bb2bs(b *bytes.Buffer) []byte {
 	}
 	b.Write(zeros[:r])
 	return b.Bytes()
+}
+
+// Br2bs extracts the backing slice from a bufio.Reader
+// The bufio.Reader can not be used and must be discarded when this function returns.
+func Br2bs(r *bufio.Reader) []byte {
+	d := doroboReader{}
+	r.Reset(&d)
+	r.ReadByte()
+	r.Reset(nil)
+	return d.b
+}
+
+type doroboReader struct {
+	io.Reader
+	b []byte
+}
+
+func (d *doroboReader) Read(buf []byte) (int, error) {
+	d.b = buf
+	return len(buf), nil
 }
